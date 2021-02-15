8885b258ca8e43a3077145aae483b4f7cdf1d9b8
bhowe111 <brendan.howe1@gmail.com>
2021-02-15 13:17:56 -0800

    added procfile

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const path = require('path');
-const photosRouter = require('./routers/photos');
-require('./db/connection');
+const express = require("express");
+const path = require("path");
+const photosRouter = require("./routers/photos");
+require("./db/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3300;
 
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
 app.use(photosRouter);
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
 
 app.listen(PORT, () => {
