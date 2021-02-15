5bdec69e27a03dfe399377b4b554fe9e291c6c04
bhowe111 <brendan.howe1@gmail.com>
2021-02-15 15:51:41 -0800

    updated index.js dirname routes

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ require("./db/connection");
 const app = express();
 const PORT = process.env.PORT || 3300;
 
-app.use(express.static(path.join(__dirname, "..", "client", "build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.use(photosRouter);
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 app.listen(PORT, () => {
